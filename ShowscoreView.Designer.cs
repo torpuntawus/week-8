cb3565825986a8ca81b4a8eaa5572e04f32e17a2
Tor.Puntawus <tor.puntawus@hotmail.com>
2015-11-10 15:02:57 +0700

    --Edit ShowScoreView --

---
+++ ShowscoreView.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace twozerofoureight
+{
+    partial class ShowscoreView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score";
+            // 
+            // lblScore2
+            // 
+            this.lblScore2.BackColor = System.Drawing.Color.Gainsboro;
+            this.lblScore2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblScore2.Location = new System.Drawing.Point(125, 22);
+            this.lblScore2.Name = "lblScore2";
+            this.lblScore2.Size = new System.Drawing.Size(216, 44);
+            this.lblScore2.TabIndex = 1;
+            this.lblScore2.Text = "0";
+            this.lblScore2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ShowscoreView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 97);
+            this.Controls.Add(this.lblScore2);
+            this.Controls.Add(this.label1);
+            this.Name = "ShowscoreView";
+            this.Text = "Showscore";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore2;
+    }
+}
